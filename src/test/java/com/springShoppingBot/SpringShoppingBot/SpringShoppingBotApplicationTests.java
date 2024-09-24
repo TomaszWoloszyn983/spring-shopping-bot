39447c3f3e92885f5e19efdc6ce3d91955c5a9d4
TomaszWoloszyn983 <tomaszwoloszyn983@gmail.com>
2024-09-24 06:59:24 +0100

    Springboot application template created

---
+++ src/test/java/com/springShoppingBot/SpringShoppingBot/SpringShoppingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springShoppingBot.SpringShoppingBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
