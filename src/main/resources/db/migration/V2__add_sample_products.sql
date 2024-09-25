e5adea5948205088b321f06b83801d4dede4ef63
TomaszWoloszyn983 <tomaszwoloszyn983@gmail.com>
2024-09-25 15:34:44 +0100

    Flyway installed. Initial migrations created

---
+++ src/main/resources/db/migration/V2__add_sample_products.sql
@@ -0,0 +1,11 @@
+INSERT INTO DT_PRODUCTS (name, type, sizeOfUnit, numOfUnits)
+VALUES 
+('Milk', 'Dairy', '1L', 2),
+('Bread', 'Bakery', '500g', 1),
+('Eggs', 'Poultry', '12 Pack', 1),
+('Apples', 'Fruit', '1kg', 3),
+('Chicken Breast', 'Meat', '500g', 2),
+('Pasta', 'Grains', '1kg', 1),
+('Tomatoes', 'Vegetable', '500g', 4),
+('Butter', 'Dairy', '250g', 1),
+('Orange Juice', 'Beverage', '1L', 1);
