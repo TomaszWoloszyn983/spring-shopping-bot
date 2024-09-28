35684fa9539e95e4657a1f22fa95e255fa5fa68f
TomaszWoloszyn983 <tomaszwoloszyn983@gmail.com>
2024-09-28 09:57:09 +0100

    Edited shoppingList.html template. Displayed list of products on the page

--- src/main/java/com/springShoppingBot/SpringShoppingBot/product/ProductController.java
+++ src/main/java/com/springShoppingBot/SpringShoppingBot/product/ProductController.java
@@ -1,13 +1,41 @@
 package com.springShoppingBot.SpringShoppingBot.product;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class ProductController {
 
     public final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @RequestMapping(("shoppingList"))
+    @GetMapping
+    public String displayShoppingList(){
+        return "shoppingList.html";
+    }
+
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    /**
+     * Passes the list of products to the shoppingList.html template
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping(path="shoppingList")
+    public String displayAllProducts(Model model){
+        model.addAttribute("products", getAllProducts());
+        return "shoppingList.html";
     }
 }
