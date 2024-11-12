850733a38f143bd517a9f11de8e9b15b6f1e9a7a
TomaszWoloszyn983 <tomaszwoloszyn983@gmail.com>
2024-11-12 07:00:31 +0000

    Created Flyway migrations to create Order, User and Role tables

--- src/main/java/com/springShoppingBot/SpringShoppingBot/security/SecurityConfig.java
+++ src/main/java/com/springShoppingBot/SpringShoppingBot/security/SecurityConfig.java
@@ -9,12 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // Use plain text passwords
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
