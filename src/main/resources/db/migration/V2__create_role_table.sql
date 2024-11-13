abd09067723c1289df903c1f7ba0cb997b5933f4
TomaszWoloszyn983 <tomaszwoloszyn983@gmail.com>
2024-11-13 07:13:09 +0000

    Another database schema reset

--- src/main/resources/db/migration/V2__create_role_table.sql
+++ src/main/resources/db/migration/V2__create_role_table.sql
@@ -2,8 +2,9 @@ DROP TABLE IF EXISTS Role;
 
 CREATE TABLE Role (
     Id SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL UNIQUE
+    Name VARCHAR(255) NOT NULL UNIQUE,
+    Description VARCHAR(255)
 );
 
-INSERT INTO Role (Name) VALUES ('ADMIN');
-INSERT INTO Role (Name) VALUES ('USER');
+INSERT INTO Role (Name, Description) VALUES ('ADMIN', 'Allows access to application settings and to manage user accounts.');
+INSERT INTO Role (Name, Description) VALUES ('USER', 'Allows access limited to using application features');
