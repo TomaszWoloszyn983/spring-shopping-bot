53a3014190014ce56a3abc71f3b6e6125c10a820
TomaszWoloszyn983 <tomaszwoloszyn983@gmail.com>
2025-03-10 11:26:55 +0000

    Hidden JWT_SECRET in environment variables storage

--- src/main/java/com/springShoppingBot/SpringShoppingBot/security/SecurityContent.java
+++ src/main/java/com/springShoppingBot/SpringShoppingBot/security/SecurityContent.java
@@ -1,5 +1,8 @@
 package com.springShoppingBot.SpringShoppingBot.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySources;
+
 public class SecurityContent {
 
     /*
@@ -8,5 +11,5 @@ public class SecurityContent {
         * 60 minutes * 10 hours = 10h.
     */
     public static final long JWT_EXPIRATION = 24 * 60 * 60 * 1000;
-    public static final String JWT_SECRET = "Bto-HGNKAX-e8J6f8DN1rRBHZJYnyVBIl5RmjHGLHa-4UB1nJbmvXLOK7sHZNWc-ANppYznLRSn-5IblVuCtl_7YXM54XvFGi58tXUEHB2uBWyKXLxvtw3q0PkOCDYDvuCmhatN77KKCn08RTx2_kVoLEaVuEtKt8EpEwhU_YsautFxBOAxfzr4ZS7lf2RMg5hy98_AYJB2fuTsZ4l42VbmFoOBPUFeWK_7c2_P8LcD3Vs0-inYO9-yNDN0Cdj9wq6C2Ksg0MGh7n5u8hKBcMueTlg3DlvXqiFUXzrJLsY_8DW4JtEenTQle8g6JO4pYV35xWa-bPfCi9GQ-hKtcUw";
+    public static String JWT_SECRET = System.getenv("JWT_SECRET_KEY");
 }
